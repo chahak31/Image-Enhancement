c3250a3af2179a98629d883d276b80edae92327b
Chahak Jadon <chahakjadon31@gmail.com>
2020-05-15 18:25:34 +0530

    added histogramEqualization.py

---
+++ histogramEqualization.py
@@ -0,0 +1,46 @@
+#Methods for Image Enhancement
+
+#Method1
+#cv2.equalizeHist(img)
+
+#Method 2
+#Contrast Limited Adaptive Histogram Equalization
+#create a CLAHE object (Arguments are optional)
+#clahe=cv2.createCLAHE()
+#clahe=cv2.createCLAHE(clipLimit=2.0, tileGridsize=(8,8))
+#apply this object to an image
+#cl1=clahe.apply(img)
+
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+img=cv.imread('night.jpg',0)
+
+##creating histogram Equalization of image
+histEq1=cv.equalizeHist(img)
+##Using CLAHE 
+#clahe=cv.createCLAHE()
+clahe=cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8)) #adjusts the histogram
+histEq2=clahe.apply(img)
+
+output=[img,histEq1, histEq2]
+titles=['Original Image','k=2','k=4']
+
+for i in range(3):
+    plt.subplot(1, 3, i+1)
+    plt.imshow(output[i], cmap='gray')
+    plt.title(titles[i])
+    plt.xticks([])
+    plt.yticks([])
+plt.show()
+#stacking images side-by-side
+#sbs=np.hstack((img,histEq))
+
+#cv.imwrite('img',sbs)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
+
+    
